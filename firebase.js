f9cb1b7a0b4837d2e5feed2908f8bf49cc8d2dd6
techbire <anshtechnical@gmail.com>
2024-07-31 21:52:37 +0530

    firebase problem solve

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics, isSupported } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,4 +19,11 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+let analytics;
+if (typeof window !== "undefined" && (await isSupported())) {
+  analytics = getAnalytics(app);
+}
+
+export { db, analytics };
